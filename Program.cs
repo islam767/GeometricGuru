ef47b1079ac0bdafd84ff51995afca8037feebca
islam767 <islomxon767@gmail.com>
2026-05-04 21:24:35 +0300

    yangi kod qushildi

--- Program.cs
+++ Program.cs
@@ -38,6 +38,98 @@ namespace GeometryGuru
             // Programın kapanmasını engellemek için
             Console.WriteLine("\nChiqish uchun bir tugmaga bosing...");
             Console.ReadKey();
+
+            
+
+
+            Console.WriteLine("--- GeometryGuru Amallar Menusi ---");
+            Console.WriteLine("1. Qo'shish (+)");
+            Console.WriteLine("2. Ayirish (-)");
+            Console.WriteLine("3. Ko'paytirish (*)");
+            Console.WriteLine("4. Bo'lish (/)");
+            Console.Write("\nBajarmoqchi bo'lgan amalingizni tanlang (1-4): ");
+
+            int tanlov = Convert.ToInt32(Console.ReadLine());
+             
+  
+            //2 Kirish qismi
+            Console.WriteLine("=== GEOMETRY GURU  ===");
+            Console.WriteLine("1. Oddiy arifmetika (+, -, *, /)");
+            Console.WriteLine("2. Chuqur (hovuz) hajmini hisoblash");
+            Console.Write("\nTanlovingizni kiriting (1 yoki 2): ");
+            string asosiyTanlov = Console.ReadLine();
+
+            // 1-YO'L: Oddiy Arifmetika
+            if (asosiyTanlov == "1")
+            {
+                Console.Write("Amalni kiriting (+, -, *, /): ");
+                string amal = Console.ReadLine();
+
+                Console.Write("1-sonni kiriting: ");
+                double s1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("2-sonni kiriting: ");
+                double s2 = Convert.ToDouble(Console.ReadLine());
+
+                // Hisoblash mantiqi
+                if (amal == "+")
+                {
+                    Console.WriteLine("Natija: " + (s1 + s2));
+                }
+                else if (amal == "-")
+                {
+                    Console.WriteLine("Natija: " + (s1 - s2));
+                }
+                else if (amal == "*")
+                {
+                    Console.WriteLine("Natija: " + (s1 * s2));
+                }
+                else if (amal == "/")
+                {
+                    // Sening asosiy sharting: 0 ga bo'lishni tekshirish
+                    if (s2 == 0)
+                    {
+                        Console.WriteLine("Xatolik: 0 ga bo'lish mumkin emas!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Natija: " + (s1 / s2));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Noto'g'ri amal kiritildi!");
+                }
+            }
+            // 2-YOL: Chuqur hajmi
+            else if (asosiyTanlov == "2")
+            {
+                Console.WriteLine("\n--- Chuqur hajmini hisoblash ---");
+                Console.Write("Uzunlik (metr): ");
+                double u = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Eni (metr): ");
+                double e = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Chuqurlik (metr): ");
+                double h = Convert.ToDouble(Console.ReadLine());
+
+                // Hajm m3 da
+                double hajm = u * e * h;
+                // Litrga o'tkazish
+                double litr = hajm * 1000;
+
+                Console.WriteLine("\n----------------------------");
+                Console.WriteLine($"Chuqur hajmi: {hajm} m3");
+                Console.WriteLine($"Ichiga sig'adigan suv: {litr} litr");
+                Console.WriteLine("----------------------------");
+            }
+            else
+            {
+                Console.WriteLine("notugri  tanlov ");
+            }
+
+            
         }
     }
 }
